39b54e2ada3c3d6ebbca7da014cf990998c7bed4
Marcolinonft <110620436+Marcolinonft@users.noreply.github.com>
2024-02-23 09:59:07 +0100

    Update AttivitaCard.jsx

--- site/src/Components/Pages/Attivita/AttivitaCard.jsx
+++ site/src/Components/Pages/Attivita/AttivitaCard.jsx
@@ -1,29 +1,29 @@
 import React from "react";
 import "./AttivitaStyle.css";
-import StreatchingIcon from "../../Icons/Streatching";
-import PullupsIcon from "../../Icons/PullupsIcon";
-import DeadliftIcon from "../../Icons/DeadliftIcon";
+import StreatchingIcon from "./Icons/AttStreIcon";
+import PullupsIcon from "./Icons/AttPullIcon";
+import DeadliftIcon from "./Icons/AttDeadIcon";
 import { NavLink } from "react-router-dom";
 
 const AttivitaCard = () => {
   const attivita = [
     {
       id: 1,
-      icon: <PullupsIcon />,
+      icon: <PullupsIcon className={"icon-card-att"} width={"300"} height={"300"}/>,
       name: "Fitness",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, placeat tempore ullam sapiente ex odio facere cupiditate iure quos",
       btn: "FITNESS",
     },
     {
       id: 2,
-      icon: <DeadliftIcon />,
+      icon: <DeadliftIcon className={"icon-card-att"} width={"300"} height={"300"}/>,
       name: "Powerlift",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, placeat tempore ullam sapiente ex odio facere cupiditate iure quos",
       btn: "POWERLIFTING",
     },
     {
       id: 3,
-      icon: <StreatchingIcon />,
+      icon: <StreatchingIcon className={"icon-card-att"} width={"300"} height={"300"}/>,
       name: "Posturale",
       desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero, placeat tempore ullam sapiente ex odio facere cupiditate iure quos",
       btn: "POSTURALE",
@@ -34,7 +34,7 @@ const AttivitaCard = () => {
     <div className="att-container">
       <div className="att-header">
         <h1 className="att-title">Attivita</h1>
-        <p className="att-desc">
+        <p className="header-desc">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero,
           placeat tempore ullam sapiente ex odio facere cupiditate iure quos
           laboriosam reiciendis, perspiciatis beatae accusantium perferendis
@@ -46,11 +46,13 @@ const AttivitaCard = () => {
         {attivita.map((item) => (
           <div className={`${"attivita" + item.id}`} key={item.id}>
             <div className="att-icon">{item.icon}</div>
-            <h4 className="att-name">{item.name}</h4>
-            <p className="att-desc">{item.desc}</p>
-            <NavLink exact to="/antonioungaro">
-              <button class="bn632-hover bn22">{item.btn}</button>
-            </NavLink>
+              <div className="info-att-container">
+                <h4 className="att-name">{item.name}</h4>
+                <p className="att-desc">{item.desc}</p>
+                <NavLink exact to="/antonioungaro">
+                  <button class="bn632-hover bn22">{item.btn}</button>
+                </NavLink>
+              </div>
           </div>
         ))}
       </div>
